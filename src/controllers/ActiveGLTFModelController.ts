27bb86753889f066bb7e4527231035415f864c10
Htrap19 <parthl@ivalueplus.com>
2026-07-03 22:55:43 +0530

    Add GLTFModelController to handle loading and ownership of Object3D

---
+++ src/controllers/ActiveGLTFModelController.ts
@@ -0,0 +1,60 @@
+import { Object3D, Scene, Box3, Vector3 } from "three";
+import { GLTFLoader, type GLTF } from "three/examples/jsm/Addons.js";
+
+export class ActiveGLTFModelController {
+    private scene: Scene;
+    private loader = new GLTFLoader();
+    private current: Object3D | null = null;
+    private currentGLTF: GLTF | null = null;
+    private scaleUnits = 10;
+
+    constructor(scene: Scene) {
+        this.scene = scene;
+    }
+    
+    load(url: string) {
+        const sceneName = url.substring(url.lastIndexOf("/") + 1);
+        const parentPath = url.substring(0, url.lastIndexOf("/"));
+        
+        if (this.current) {
+            this.scene.remove(this.current);
+        }
+
+        this.loader.setPath(parentPath + "/");
+        this.loader.load(sceneName, (data) => {
+            this.currentGLTF = data;
+            this.current = data.scene;
+            this.scene.add(this.current);
+            this.center(this.current);
+        });
+    }
+
+    center(model: Object3D) {
+        model.position.set(0, 0, 0);
+
+        const box = new Box3().setFromObject(model);
+        const center = box.getCenter(new Vector3());
+        const size = box.getSize(new Vector3());
+
+        const maxAxis = Math.max(size.x, size.y, size.z);
+        const scale = this.scaleUnits / maxAxis;
+
+        // Scale first
+        model.scale.setScalar(scale);
+
+        // Recompute the bounding box after scaling
+        box.setFromObject(model);
+        box.getCenter(center);
+
+        // Then center it
+        model.position.sub(center);
+    }
+
+    getObject3D() {
+        return this.current;
+    }
+
+    getGLTF() {
+        return this.currentGLTF;
+    }
+}
--- src/scene/MainScene.ts
+++ src/scene/MainScene.ts
@@ -1,36 +1,38 @@
 import {
     PerspectiveCamera,
     Scene,
-    TorusGeometry,
-    MeshStandardMaterial,
-    Mesh,
-    PointLight,
-    AmbientLight,
+    DirectionalLight,
     WebGLRenderer,
-    PointLightHelper,
-    GridHelper
+    GridHelper,
+    DirectionalLightHelper,
+    AmbientLight
 } from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 import { BaseScene } from "./BaseScene";
+import { ActiveGLTFModelController } from "../controllers/ActiveGLTFModelController";
 
 export class MainScene extends BaseScene {
     scene = new Scene();
     camera = new PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 
     // Scene Elements
-    geometry = new TorusGeometry( 10, 3, 16, 100 );
-    material = new MeshStandardMaterial({ color: 0xFF6347 });
-    torus = new Mesh( this.geometry, this.material );
-    pointLight = new PointLight(0xFFFFFF);
-    ambientLight = new AmbientLight(0xFFFFFF);
+    keyLight = new DirectionalLight(0xffffff, 2);
+    fillLight = new DirectionalLight(0x99bbff, 0.5);
+    rimLight = new DirectionalLight(0xffffff, 1);
+    ambientLight = new AmbientLight(0xFFFFFF, 0.4);
 
     // Helpers
-    pointLightHelper = new PointLightHelper(this.pointLight);
+    keyLightHelper = new DirectionalLightHelper(this.keyLight)
+    fillLightHelper = new DirectionalLightHelper(this.fillLight)
+    rimLightHelper = new DirectionalLightHelper(this.rimLight)
     gridHelper = new GridHelper(200, 50);
 
     // Contols
     private controls: OrbitControls;
+
+    // Controllers
+    private activeModelController = new ActiveGLTFModelController(this.scene);
 
     constructor(renderer: WebGLRenderer) {
         super();
@@ -38,15 +40,23 @@ export class MainScene extends BaseScene {
         this.controls = new OrbitControls(this.camera, renderer.domElement);
         
         this.camera.position.setZ(50);
-        this.pointLight.intensity = 100;
+        
+        this.keyLight.position.set(10, 20, 10);
+        this.fillLight.position.set(-10, 5, 5);
+        this.rimLight.position.set(0, 10, -15);
 
         this.scene.add(
-            this.torus,
-            this.pointLight,
+            this.keyLight,
+            this.fillLight,
+            this.rimLight,
             this.ambientLight,
-            this.pointLightHelper,
-            this.gridHelper
+            this.gridHelper,
+            this.keyLightHelper,
+            this.fillLightHelper,
+            this.rimLightHelper,
         );
+
+        this.activeModelController.load("assets/gltf-models/stylized_stone_cube/scene.gltf");
     }
 
     update(delta: number): void {
